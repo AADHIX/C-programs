7c6aa9291f49a1307acec6444d7e8ebcf199265b
ADARSH AJAY <99534398+AADHIX@users.noreply.github.com>
2025-07-09 17:16:21 +0530

    Create SumCalcfunc.c
    Understanding core basic function

---
+++ hello.c/SumCalcfunc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void Calculatesum(){
+
+    int a = 5;
+    int b = 10;
+     int  sum = a + b;
+
+     printf("The sum of two number is %d", sum);
+
+
+
+
+
+}
+
+int main()
+
+{
+
+    Calculatesum();
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
