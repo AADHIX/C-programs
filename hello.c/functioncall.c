2692153dad8e16cfbe113048ee10ca0cdab70f6b
ADARSH AJAY <99534398+AADHIX@users.noreply.github.com>
2025-07-09 17:07:20 +0530

    Create functioncall.c
    Edited for adjusting the return type

---
+++ hello.c/functioncall.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void myClass(){
+
+
+    printf("Enter the name\n ");
+
+
+}
+
+
+int main()
+
+{
+
+    myClass();
+    myClass();
+    myClass();
+
+
+
+    return 0;
+}
+
+
+
+
+
